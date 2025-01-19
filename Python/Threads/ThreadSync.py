cef21c31da10c21bcbf9fbaa00223bac9e0a413a
Pranjal Parmar <cube@Pranjals-MacBook-Pro.local>
2025-01-19 11:45:03 +0530

    new solutions

---
+++ Python/Threads/ThreadSync.py
@@ -0,0 +1,8 @@
+"""
+Thread Synchronization or Mutual Exclusion and Locks
+
+Why we need thread synchronication or Mutual Exclusion ( if one person has it, another can't)?
+>> When multiple threads are running in a program, they may access the same resource at the same time. This may lead to data inconsistency.
+>> To avoid this, we need to synchronize the threads. This is called thread synchronization.
+
+"""
